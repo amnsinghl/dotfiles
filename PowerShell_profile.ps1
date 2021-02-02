5b2fb55f781a3e7a3a0107aa6ed82204e395fc26
Aman Singhal <helixinverse@gmail.com>
2021-02-02 22:15:13 +0530

    Create PowerShell_profile.ps1

---
+++ PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme PowerlinePlus
+Import-Module PSReadLine
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+
+#Set the color for Prediction (auto-suggestion)
+#Set-PSReadlineOption -Colors @{Prediction = 'DarkGreen' }
+#Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
+
+$installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
+Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+Function devshellfunc {Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation}
+Set-Alias -Name devshell -Value devshellfunc
+
+Function dmsshellfunc {C:\Torus\TorusClient.ps1 -Mode connect -ServiceInstance MultiTenant -UserType OCE -DMS}
+Set-Alias -Name dms -Value dmsshellfunc
